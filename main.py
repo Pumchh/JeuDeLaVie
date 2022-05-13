f296b033791bc9a1ff115bfacf753bd235399750
Pumchh <47301060+Pumchh@users.noreply.github.com>
2022-05-13 14:33:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -15,20 +15,30 @@ def printMap(map:list) -> None:
     :param map: map to print
     :return: None
     """
+    cara = ""
     for i in map:
-        print(i)
+        cara += "\n"
+        for j in i:
+            if j == 0:
+                cara += "-"
+            elif j == 1:
+                cara += "*"
+    print(cara)
 
-
-def setBlock(map:list, x:int, y:int) -> None:
-
+def setState(map:list, x:int, y:int) -> None:
+    """
+    :param map: map where you set the new state
+    :param x: coordinate x
+    :param y: coordinate y
+    :return: None
+    """
     if map[x][y] == 0:
         map[x][y] = 1
     else:
         map[x][y] = 0
- 
 
 
-       
+"""       
 def verifAroundBlock(map:list, x:int, y:int, hight:int=1, width:int=1) -> int:
     coordoX = x - width 
     coordoY = y - hight 
@@ -42,32 +52,74 @@ def verifAroundBlock(map:list, x:int, y:int, hight:int=1, width:int=1) -> int:
             if coordoX >= 0 and coordoY >= 0 and coordoX < XMap and coordoY < YMap:
                 if map[coordoX][coordoY] == 1 and i != j:
                     count += 1
-
     return count
+"""
+
+# AJouter conditions bords du tableau
+def verifAroundBlock(map:list, x:int, y:int) -> int:
+    counter = 0
+
+    if y > 1 and x > 1:
+        if map[y - 1][x - 1] == 1:
+            counter += 1
+
+    if x > 1:
+        if map[y][x - 1] == 1:
+            counter += 1
+
+    if y < YMap-1 and x > 1:
+        if map[y + 1][x - 1] == 1:
+            counter += 1
+
+    if y > 1:
+        if map[y - 1][x] == 1:
+            counter += 1
+
+    if y < YMap-1:
+        if map[y + 1][x] == 1:
+            counter += 1
+
+    if y > 1 and x < XMap-1:
+        if map[y - 1][x + 1] == 1:
+            counter += 1
+
+    if x < XMap-1:
+        if map[y][x + 1] == 1:
+            counter += 1
+
+    if x < XMap-1:
+        if map[y + 1][x + 1] == 1:
+            counter += 1
+
+    return counter
 
 
+def rules(map, x, y):
 
-def regles():
-    pass
+    if map[x][y] == 0:
+        if verifAroundBlock(map, x, y) == 3:
+            setState(map, x, y)
+
+    elif map[x][y] == 1:
+        if verifAroundBlock(map, x, y) <= 2 or verifAroundBlock(map, x, y) >= 4:
+            setState(map, x, y)
+
+        elif verifAroundBlock(map, x, y) == 2 or verifAroundBlock(map, x, y) == 3:
+            #pas de changement
+            pass
+    else:
+        print("Erreur, ne sait pas quoi faire aux coord ({},{})".format(x, y))
 
 
-XMap = 10
-YMap = 10
+XMap = 6
+YMap = 6
 
 map = genMap(XMap, YMap)
 
 printMap(map)
 
 
-setBlock(map, 4, 4)
-setBlock(map, 4, 5)
-setBlock(map, 4, 6)
-setBlock(map, 5, 4)
-setBlock(map, 5, 5)
-setBlock(map, 5, 6)
-setBlock(map, 6, 4)
-setBlock(map, 6, 5)
-setBlock(map, 6, 6)
+setState(map, 3, 3)
 
 print("\n")
 
